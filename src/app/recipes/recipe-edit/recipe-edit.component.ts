42d73c805461263290bac9ba2cb5606f352933e5
Chris Kozanecki <chris.kozanecki@gmail.com>
2016-12-13 13:38:34 -0600

    Recipe edit & detail complete form binding

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {RecipeService} from "../recipe.service";
 import {Subscription} from "rxjs";
 import {Recipe} from "../shared/recipe";
@@ -19,7 +19,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
   constructor(private route: ActivatedRoute,
               private recipeService: RecipeService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private router:Router) {
   }
 
   ngOnInit() {
@@ -52,8 +53,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
       for(let i=0; i < this.recipe.ingredients.length; i++){
         recipeIngredients.push(
           new FormGroup({
-            name: new FormControl(this.recipe.ingredients[i].ingredientName, Validators.required),
-            amount: new FormControl(this.recipe.ingredients[i].quantity, [Validators.required, Validators.pattern("\\d+")])
+            ingredientName: new FormControl(this.recipe.ingredients[i].ingredientName, Validators.required),
+            quantity: new FormControl(this.recipe.ingredients[i].quantity, [Validators.required, Validators.pattern("\\d+")])
           })
         );
       }
@@ -68,5 +69,37 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
       description: [recipeContent, Validators.required],
       ingredients: recipeIngredients
     });
+  }
+
+  onSubmit(){
+    const newRecipe = this.recipeForm.value;
+    if(this.isNew){
+      this.recipeService.addRecipe(newRecipe);
+    } else {
+      this.recipeService.editRecipe(this.recipe, newRecipe)
+    }
+    this.navigateBack();
+  }
+
+  onCancel(){
+    this.navigateBack();
+  }
+
+  onAddItem(){
+    (<FormArray>this.recipeForm.controls['ingredients']).push(
+      new FormGroup({
+        ingredientName: new FormControl('', Validators.required),
+        quantity: new FormControl('', [Validators.required, Validators.pattern("\\d+")])
+      })
+    );
+  }
+
+  onRemoveItem(index:number){
+    (<FormArray>this.recipeForm.controls['ingredients']).removeAt(index);
+  }
+
+
+  private navigateBack(){
+    this.router.navigate(['../']);
   }
 }
