b17b3c6eb8eebd1a4be00c13c816c382823a2ce7
Chris Kozanecki <chris.kozanecki@gmail.com>
2016-12-20 10:16:07 -0600

    Lazy Loading implemented on Recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
 import {DropdownDirective} from "./dropdown.directive";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {RecipeService} from "./recipes/recipe.service";
-import {RouterModule} from "@angular/router";
-import {APP_ROUTES} from "./app.routing";
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start.component';
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {AppRouting} from "./app.routing";
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
     DropdownDirective,
-    RecipeEditComponent,
-    RecipeStartComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpModule,
-    RouterModule.forRoot(APP_ROUTES)
+    AppRouting,
+    ShoppingListModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,11 @@
-import {Routes} from "@angular/router";
-import {RecipesComponent} from "./recipes/recipes.component";
+import {Routes, RouterModule} from "@angular/router";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RECIPE_ROUTES} from "./recipes/recipes.routes";
+import {HomeComponent} from "./home.component";
 
-export const APP_ROUTES: Routes = [
-  { path: 'recipes', component: RecipesComponent, children: RECIPE_ROUTES},
-  { path: 'shopping-list', component:ShoppingListComponent},
-  { path: '', redirectTo: '/recipes', pathMatch:'full'}
+const APP_ROUTES: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'recipes', loadChildren: 'app/recipes/recipes.module#RecipesModule'},
+  { path: 'shopping-list', component:ShoppingListComponent}
 ];
+
+export const AppRouting = RouterModule.forRoot(APP_ROUTES);
